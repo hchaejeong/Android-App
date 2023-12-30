4aa9c12a6c0173b134cba5907729698dd33ae097
hchaejeong <hchaejeong@kaist.ac.kr>
2023-12-30 13:10:51 +0900

    gallery 탭 구현 - 갤러리 앱이랑 연동 시켜놓기 - 사진 불러오기 기능

---
+++ app/src/main/java/com/example/myapplication/ImageDetailActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+public class ImageDetailActivity extends AppCompatActivity {
+    String imagePath;
+    private ImageView imageView;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_detail);
+
+        imagePath = getIntent().getStringExtra("imagePath");
+        imageView = findViewById(R.id.idIVImage);
+        //using this for zoom in and zoom out of the image
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
+        File imageFile = new File(imagePath);
+
+        if (imageFile.exists()) {
+            Picasso.get().load(imageFile).placeholder(R.drawable.ic_launcher_background).into(imageView);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+            imageView.setScaleX(mScaleFactor);
+            imageView.setScaleY(mScaleFactor);
+            return true;
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/fragments/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/GalleryFragment.java
@@ -1,66 +1,102 @@
 package com.example.myapplication.fragments;
 
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.myapplication.GalleryAdapter;
 import com.example.myapplication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GalleryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
 public class GalleryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ArrayList<String> imagePaths;
+    private RecyclerView imagesRV;
+    private GalleryAdapter galleryAdapter;
 
     public GalleryFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GalleryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GalleryFragment newInstance(String param1, String param2) {
-        GalleryFragment fragment = new GalleryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 200) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getImagePath();
+            }
+            else {
+                Toast.makeText(requireContext(), "Permission Denied.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        imagesRV = view.findViewById(R.id.RVImages);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imagePaths = new ArrayList<>();
+        requestPermissions();
+        prepareRecyclerView();
+    }
+
+    private void requestPermissions() {
+        int permissionResult = ContextCompat.checkSelfPermission(requireContext(), READ_EXTERNAL_STORAGE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && permissionResult != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{READ_EXTERNAL_STORAGE}, 200);
+        } else {
+            getImagePath();
+        }
+    }
+
+    private void prepareRecyclerView() {
+        galleryAdapter = new GalleryAdapter(requireContext(), imagePaths);
+        GridLayoutManager manager = new GridLayoutManager(requireContext(), 4);
+        imagesRV.setLayoutManager(manager);
+        imagesRV.setAdapter(galleryAdapter);
+    }
+
+    public void getImagePath() {
+        boolean galleryPresent = android.os.Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+
+        if (galleryPresent) {
+            final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};
+            final String orderBy = MediaStore.Images.Media._ID;
+
+            Cursor cursor = requireContext().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, orderBy);
+
+            int count = cursor.getCount();
+            for (int i = 0; i < count; i++) {
+                cursor.moveToPosition(i);
+                int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                imagePaths.add(cursor.getString(dataColumnIndex));
+            }
+            galleryAdapter.notifyDataSetChanged();
+            cursor.close();
+        }
     }
 }
