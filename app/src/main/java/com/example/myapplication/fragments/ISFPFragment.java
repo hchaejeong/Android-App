ad36a9134186bd762877831d2db40d473b6091a1
franktome <yunhs139@dgist.ac.kr>
2024-01-01 20:22:34 +0900

    Tap3 모든 화면을 fragment로 바꿈, "처음으로"버튼을 눌르면 tap3 초기화면으로 돌아가고 네비게이션 바도 바뀌지 않음

--- app/src/main/java/com/example/myapplication/fragments/ISFPFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/ISFPFragment.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
-import com.example.myapplication.Tap3_Page1;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 
--- app/src/main/java/com/example/myapplication/fragments/Tap3_Page4Fragment.java
+++ app/src/main/java/com/example/myapplication/fragments/Tap3_Page4Fragment.java
@@ -1,6 +1,5 @@
 package com.example.myapplication.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +11,10 @@ import androidx.fragment.app.Fragment;
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.SharedViewModel;
-import com.example.myapplication.Tap3_Page1;
-import com.example.myapplication.Tap3_Page2;
 
 import androidx.lifecycle.ViewModelProvider;
-import androidx.fragment.app.Fragment;
 
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Tap3_Page4Fragment extends Fragment {
