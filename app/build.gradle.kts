69f9bc8548b6754f2d5501b356ef4ab1cf95abe8
franktome <yunhs139@dgist.ac.kr>
2024-01-01 23:58:44 +0900

    Tap3 질문지 오타 수정 및 Tap3_resultPageFragment 페이지에서 선물상자 커지면서 색종이 날리는 애니메이션 추가함.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 }
 
 dependencies {
-
+    implementation("nl.dionsegijn:konfetti:1.1.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("androidx.exifinterface:exifinterface:1.3.7")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("nl.dionsegijn:konfetti:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
