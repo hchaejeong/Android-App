68d5054483a71159cea0ccd1b109ded9dc1307fe
hchaejeong <hchaejeong@kaist.ac.kr>
2024-01-02 21:23:51 +0900

    이미지 창 기능 추가: - 카메라랑 연동해서 사진 추가 - 최근에 추가된 순서로 사진 띄우기

--- app/src/main/java/com/example/myapplication/fragments/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/GalleryFragment.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +23,8 @@ import android.widget.Toast;
 
 import com.example.myapplication.GalleryAdapter;
 import com.example.myapplication.R;
+import com.example.myapplication.TakePicture;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -27,8 +32,10 @@ import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class GalleryFragment extends Fragment {
 
+    private GalleryAdapter myAdapter;
     private ArrayList<String> imagePaths;
     private RecyclerView imagesRV;
+    private static final int TAKE_PICTURE_REQUEST_CODE = 123;
 
     public GalleryFragment() {
         // Required empty public constructor
@@ -40,7 +47,41 @@ public class GalleryFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
         imagesRV = view.findViewById(R.id.RVImages);
+
+        FloatingActionButton takePictureButton = view.findViewById(R.id.idImageAdd);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(requireActivity(), TakePicture.class);
+                startActivityForResult(i, TAKE_PICTURE_REQUEST_CODE);
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TAKE_PICTURE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            if (data != null && data.hasExtra("capturedImageUri")) {
+                String capturedImageUri = data.getStringExtra("capturedImageUri");
+                imagePaths.add(0, capturedImageUri);
+                refreshGallery();
+            }
+        }
+    }
+
+    private void refreshGallery() {
+        imagePaths.clear();
+        getImagePath();
+
+        if (myAdapter != null) {
+            myAdapter.notifyDataSetChanged();
+        }
+
+        Log.d("GalleryFragment", "Image paths: " + imagePaths.toString());
     }
 
     @Override
@@ -48,6 +89,24 @@ public class GalleryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         imagePaths = new ArrayList<>();
         requestPermissions();
+
+        myAdapter = new GalleryAdapter(requireContext(), imagePaths);
+        GridLayoutManager manager = new GridLayoutManager(requireContext(), calculateSpanCount());
+        imagesRV.setLayoutManager(manager);
+        imagesRV.setAdapter(myAdapter);
+        imagesRV.setHasFixedSize(false);
+
+        myAdapter.notifyDataSetChanged();
+    }
+
+    private int calculateSpanCount() {
+        int totalImages = imagePaths.size();
+        int defaultSpanCount = 4;
+
+        int blankSpaces = defaultSpanCount - (totalImages % defaultSpanCount);
+        int spanCount = Math.max(blankSpaces, defaultSpanCount);
+
+        return spanCount;
     }
 
     private void requestPermissions() {
@@ -55,7 +114,7 @@ public class GalleryFragment extends Fragment {
         if (permissionResult != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(requireActivity(), new String[]{READ_EXTERNAL_STORAGE}, 200);
         } else {
-            getImagePath();
+            refreshGallery();
         }
     }
 
@@ -64,7 +123,7 @@ public class GalleryFragment extends Fragment {
 
         if (galleryPresent) {
             final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};
-            final String orderBy = MediaStore.Images.Media._ID;
+            final String orderBy = MediaStore.Images.Media.DATE_TAKEN + " DESC";
 
             Cursor cursor = requireContext().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, orderBy);
 
@@ -74,10 +133,12 @@ public class GalleryFragment extends Fragment {
                     imagePaths.add(cursor.getString(dataColumnIndex));
                 }
 
-                GalleryAdapter myAdapter = new GalleryAdapter(requireContext(), imagePaths);
-                GridLayoutManager manager = new GridLayoutManager(requireContext(), 4);
+                myAdapter = new GalleryAdapter(requireContext(), imagePaths);
+                GridLayoutManager manager = new GridLayoutManager(requireContext(), calculateSpanCount());
                 imagesRV.setLayoutManager(manager);
                 imagesRV.setAdapter(myAdapter);
+                imagesRV.setHasFixedSize(false);
+
                 myAdapter.notifyDataSetChanged();
             }
         }
